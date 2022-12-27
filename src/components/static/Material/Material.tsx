ca4d850a3b02d3d11641c183a803ce8462affe6f
Andrei Nascimento <andrei.lima.nas@outlook.com>
2022-12-26 21:19:41 -0300

    Creation of material component to include study books

---
+++ src/components/static/Material/Material.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+function Material() {
+    return(
+        <Grid container className='material'>
+            
+        </Grid>
+    )
+}
+
+export default Material;
